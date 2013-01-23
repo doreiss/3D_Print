ab95da7c29f0368bb5386377327b6616352d1d89
doreiss <dominic.reiss@gmail.com>
2013-01-23 10:44:58 +0000

    Update Lattice.cpp

--- Lattice.cpp
+++ Lattice.cpp
@@ -12,10 +12,10 @@ Lattice::Lattice(void) {
 
 Lattice::Lattice(int rows, int columns, int init) {
 	vector<int> row; 
-	for(int i = 0; i < rows; i++) {
+	for(int i = 0; i < columns; i++) {
 		row.push_back(init);
 	}
-	for(int i = 0; i < columns; i++) {
+	for(int i = 0; i < rows; i++) {
 		values.push_back(row);
 	}
 }
@@ -33,3 +33,23 @@ Lattice::Lattice(char* file) {
 		values.push_back(row);
 	}	
 }
+
+void Lattice::setElement(int rowInd, int colInd, int init) {
+	values[rowInd][colInd] = init; 
+}
+
+void Lattice::setSubLattice(int bRowInd, int eRowInd, int bColInd, int eColInd, int init) {
+	int rows = 0, columns = 0; 
+	columns = values.size(); 
+	if (columns > 0) {
+		rows = values[0].size();
+	}
+	if (((eRowInd - bRowInd + 1) >= rows) && ((eColInd - bColInd + 1) >= columns)) {
+		for (int i = browInd; i <= eRowInd; i++ ){
+			for(int j = bColInd; j <= eColInd; j++) {
+				setElement(i,j,init)
+			}
+		}
+	}
+}
+
