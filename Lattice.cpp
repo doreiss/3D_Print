7b59dfd95d8172002158e1b8f012abadcb66dad9
doreiss <dominic.reiss@gmail.com>
2013-01-23 10:20:03 +0000

    Update Lattice.cpp

--- Lattice.cpp
+++ Lattice.cpp
@@ -1,7 +1,35 @@
 #include "Lattice.h"
-
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std; 
 
 Lattice::Lattice(void) {
-	values.push_back(0);
+	vector<int> emptyrow; 
+	emptyrow.push_back(0);
+	values.push_back(emptyrow); 
+}
+
+Lattice::Lattice(int rows, int columns, int init) {
+	vector<int> row; 
+	for(int i = 0; i < rows; i++) {
+		row.push_back(init);
+	}
+	for(int i = 0; i < columns; i++) {
+		values.push_back(row);
+	}
+}
+
+Lattice::Lattice(char* file) {
+	ifstream ifs(file);
+	int fileVal; 
+	string line; 
+	while(std::getline(ifs,line)) {
+		std::istringstream iss(line);
+		vector<int> row;
+		while(iss >> fileVal) {
+			row.push_back(fileVal);
+		}
+		values.push_back(row);
+	}	
 }
