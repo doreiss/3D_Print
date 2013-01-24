e963e00e81994e6fcfa876a23e6108aea6532535
doreiss <dominic.reiss@gmail.com>
2013-01-24 12:06:18 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int main() {
 	Lattice test(5,5,1);
-	test.setElementNeighbours(); 
 	test.setSubLattice(1,3,1,3,0);
-	LatElem testElem = test.values[1][1];
 	for (int i = 0; i < 8; i++) {
-		 cout << testElem.getNValue(i) << "\t"; 
+		 cout << test.getElement(1,1).getNValue(i) << "\t"; 
 	}
 	cout << endl; 
 
