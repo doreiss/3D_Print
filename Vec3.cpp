356c6171708658dcc795394425072915d54d108a
Josh <joshprice1992@gmail.com>
2013-02-07 15:03:09 +0000

    Added comments to Vec3.cpp/.h

--- Vec3.cpp
+++ Vec3.cpp
@@ -1,41 +1,53 @@
 #include "Vec3.h"
 
+//Default constructor 0,0,0
 Vec3::Vec3(void) {
 	x = 0; 
 	y = 0; 
 	z = 0; 
-	int pos = 0; 
+	int pos = 0; //how is this used? surely it does not exist outside the braces?
 }
 
+//Constructor for 3 given doubles: x, y, & z
 Vec3::Vec3(double xInit, double yInit, double zInit) { 
 	x = xInit; 
 	y = yInit; 
 	z = zInit; 
-	int pos = 0; 
+	int pos = 0; //how is this used? surely it does not exist outside the braces?
 }
 
+//Return x
 double Vec3::getX(void) {
 	return x; 
 }
+
+//Return y
 double Vec3::getY(void) { 
 	return y; 
 } 
+
+//Return z
 double Vec3::getZ(void) { 
 	return z; 
 } 
 
+//Set x
 void Vec3::setX(double xInit) { 
 	x = xInit; 
 }
 
+//Set y
 void Vec3::setY(double yInit) { 
 	y = yInit;
 }
 
+//Set z
 void Vec3::setZ(double zInit) { 
 	z = zInit; 
 }
 
+//Return true if the difference with this and another given vector is greater than a given resolution
+//Applies to all data members separately
 bool Vec3::withinResolution(Vec3 other, double resolution) { 
 	bool condX = (abs(x - other.getX()) > resolution);
 	bool condY = (abs(y - other.getY()) > resolution);
--- Vec3.h
+++ Vec3.h
@@ -2,19 +2,31 @@
 #include <vector> 
 #include <iostream> 
 
+//3D Vector Class of doubles x, y, z
 class Vec3
 {
 public:
-	Vec3(void); 
+	//Constructors
+	//Default constructor
+	Vec3(void);
+	//Construct a vec3 of 3 given doubles: x, y, z.
 	Vec3(double,double,double); 
+	//Return x
 	double getX(void); 
-	double getY(void); 
+	//Return y
+	double getY(void);
+	//Return z
 	double getZ(void);
-	void setX(double); 
-	void setY(double); 
+	//Set x
+	void setX(double);
+	//Set y
+	void setY(double);
+	//Set z
 	void setZ(double); 
+	//Resolution check (Return true if difference between 2 vectors is greater than given double (for all elements))
 	bool withinResolution(Vec3,double); 
 private:
+	//Cartesian coordinates
 	double x;
 	double y; 
 	double z;
